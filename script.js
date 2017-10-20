8cdbec22985adce501cdf9f8c53f9c5a9c036fae
Brandon Klotz <brandonklotz@icloud.com>
2017-10-20 16:02:24 -0400

    Update frontend

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
-function riddle() {
-  var text;
-  var correctAnswer = "your name";
-  var correctAnswerTwo = "Your name";
-  var userInput = prompt('What is the answer?');
-  switch (userInput) {
-    case correctAnswer:
-      text = userInput + ' is right! GOOD Job!';
-      document.getElementById("responder").style.color = "green";
-      break;
-    case correctAnswerTwo:
-      text = userInput + ' is right! GOOD Job!';
-      document.getElementById("responder").style.color = "green";
-      break;
-    case "":
-      text = 'Please enter a guess.';
-      document.getElementById("responder").style.color = "red";
-      break;
-    default:
-      text = userInput + ' is not correct, try again..';
-      document.getElementById("responder").style.color = "red";
-      break;
+function getRandomInt() {
+  var min = 0;
+  var max = 5
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+
+function initGame() {
+
+  var gamePrompt = prompt ("Do you dare enter the Dungeon to face Grant Chirpus?");
+  var consent = "yes";
+  var checkGamePrompt = gamePrompt === consent;
+  var playFalse = "Rerun code to play game.";
+
+  if (checkGamePrompt === true) {
+    userName = prompt("Enter your wizarding name!");
+
+    var grant = "Grant Chirpus";
+    var userWins = 0;
+    var pointsNeededToWin = 3;
+
+    var grantHealth = 10;
+    var userHealth = 40;
+    var healthDepleted = 0;
+
+    console.log(userName + " enters the dungeon...");
+
+    while (userWins < pointsNeededToWin && userHealth > 0) {// Rounds needed to win
+
+      while (userHealth >= healthDepleted && grantHealth >= healthDepleted) {
+
+        var grantAttack = getRandomInt();
+        var userAttack = getRandomInt();
+
+        grantHealth = grantHealth - userAttack;
+        userHealth = userHealth - grantAttack;
+
+        console.log(userName + ' has ' + userHealth + ' health..');
+        console.log(grant + ' has ' + grantHealth + ' health..');
+
+        }
+
+        console.log('round over')
+        userWins++;
+        grantHealth = 10;
+
+      }
+
+    if (userHealth < healthDepleted) {
+      console.log(userName + ' Lost');
+    } else {
+      console.log(grant + ' Lost');
+    }
+    console.log('game over');
+
+  } else {
+    console.log('Rerun code to play.')
   }
-  document.getElementById("responder").innerHTML = text;
-  return;
 }
