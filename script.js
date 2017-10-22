b410e028e319665b468ecdf1e12e95a1499007f9
Brandon Klotz <brandonklotz@icloud.com>
2017-10-22 18:56:10 -0400

    added css styling and started building out other buttons

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getRandomInt() {
+var grant = 'Grant Chirpus';
+var userWins = 0;
+var pointsNeededToWin = 3;
+
+var grantHealth = 10;
+var userHealth = 40;
+var healthDepleted = 0;
+
+function getDamage() {
   var min = 0;
   var max = 5
   min = Math.ceil(min);
@@ -6,56 +14,67 @@ function getRandomInt() {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+function startCombat() {
 
-function initGame() {
+  while (userWins < pointsNeededToWin && userHealth > 0) {// Rounds needed to win
 
-  var gamePrompt = prompt ("Do you dare enter the Dungeon to face Grant Chirpus?");
-  var consent = "yes";
-  var checkGamePrompt = gamePrompt === consent;
-  var playFalse = "Rerun code to play game.";
+    while (userHealth >= healthDepleted && grantHealth >= healthDepleted) {
 
-  if (checkGamePrompt === true) {
-    userName = prompt("Enter your wizarding name!");
+      if (window.confirm('Would you like to continue Attacking?')) {
+        console.log(userName + ' decides to attack.');
+      } else {
+        console.log(userName + ' has ended the game.')
+        return;
+      }
 
-    var grant = "Grant Chirpus";
-    var userWins = 0;
-    var pointsNeededToWin = 3;
+      var grantAttack = getDamage();
+      var userAttack = getDamage();
 
-    var grantHealth = 10;
-    var userHealth = 40;
-    var healthDepleted = 0;
+      grantHealth = grantHealth - userAttack;
+      userHealth = userHealth - grantAttack;
 
-    console.log(userName + " enters the dungeon...");
-
-    while (userWins < pointsNeededToWin && userHealth > 0) {// Rounds needed to win
-
-      while (userHealth >= healthDepleted && grantHealth >= healthDepleted) {
-
-        var grantAttack = getRandomInt();
-        var userAttack = getRandomInt();
-
-        grantHealth = grantHealth - userAttack;
-        userHealth = userHealth - grantAttack;
-
-        console.log(userName + ' has ' + userHealth + ' health..');
-        console.log(grant + ' has ' + grantHealth + ' health..');
-
-        }
-
-        console.log('round over')
-        userWins++;
-        grantHealth = 10;
+      console.log(userName + ' has ' + userHealth + ' health..');
+      console.log(grant + ' has ' + grantHealth + ' health..');
 
       }
 
-    if (userHealth < healthDepleted) {
-      console.log(userName + ' Lost');
-    } else {
-      console.log(grant + ' Lost');
-    }
-    console.log('game over');
+      console.log('round over')
+      userWins++;
+      grantHealth = 10;
 
+    }
+
+  if (userHealth < healthDepleted) {
+    console.log(userName + ' Lost');
   } else {
-    console.log('Rerun code to play.')
+    console.log(grant + ' Lost');
   }
+  console.log('game over');
+
+  grantHealth = 10;
+  userHealth = 40;
+  userWins = 0;
+
+}
+
+
+function startGame() {
+
+  var gameStartPrompt = prompt ('Do you dare enter the Dungeon?');
+  var consent = 'yes';
+  var checkGamePrompt = gameStartPrompt === consent;
+  var playFalse = 'Rerun code to play game.';
+
+  if (checkGamePrompt !== true) {
+
+      console.log(playFalse);
+
+    } else {
+      userName = prompt('Enter your wizarding name!');
+
+      console.log(userName + ' enters the dungeon...');
+
+      startCombat();
+    }
+
 }
