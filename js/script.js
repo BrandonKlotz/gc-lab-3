e5d8ef4c824372127ddf9a0dc506fb91d9814974
Sloth I <github@slothwerks.com>
2017-10-24 15:24:07 -0400

    Sloth's Updated Battle Script w/Messages

--- js/script.js
+++ js/script.js
@@ -1,80 +1,166 @@
-var grant = 'Grant Chirpus';
-var userWins = 0;
-var pointsNeededToWin = 3;
 
-var grantHealth = 10;
-var userHealth = 40;
-var healthDepleted = 0;
+// When the awesome code below is complete, wrap it into a self-executing function for use on the website.
 
-function getDamage() {
-  var min = 0;
-  var max = 5
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+// Variable game parameters
+
+var userHealthDefault = 40; // Total health for the game
+var grantHealthDefault = 10; // This is health *per round*
+var grantName = "the Almighty Grant"; // Change this to revise Grant's name throughout the script.
+var totalRoundsNeededToWin = 3;
+var minDamage = 1;
+var maxDamage = 5;
+var gameWinner; // This is the one variable we're passing out of the startGame() battle code and therefore needs to be declared globally.
+var doYouWantToContinue; // This also must be a global variable, as it is used to determine some of the post-game text.
+
+// This Lab requires the start of the game and the battle loop itself to be within functions.
+
+if (startGame() === true) {
+  
+	var userName = prompt("Please enter your Wizard name.");
+
+	startCombat(); // startCombat includes the entire battle sequence loop from start to finish.
+
+	// Responses to winner declaration
+
+	if (doYouWantToContinue === false) {
+		console.log("Sharpen up those battle skills and come back when you're ready, " + userName + "!");
+	} else if (gameWinner == grantName) {
+		console.log("You have been defeated in heated battle.  Better luck next time, " + userName + "!");
+	} else if (gameWinner == userName) {
+		console.log("Congrats!  " + grantName + " has been utterly defeated.  We knew you had it in you, " + userName + "; well done indeed!");
+	} else {
+		console.log("Wait a minute... we can't figure out what happened here.  Did " + gameWinner + " win?  This code is seriously flawed!");
+	}	
+
+} else {
+
+  console.log("This script will need to be re-run in order to play.");
+  
 }
+
+// getDamage() generates a random integer between min and max.
+
+function getDamage(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// startGame() determines if the user wishes to play the game.
+
+function startGame() {
+	var playResponse = window.prompt("Do you dare to enter the dungeon of " + grantName + "? (Y/N)", "Y");
+	if (playResponse.toLowerCase() == "y") {
+		window.alert("So; you believe you can defeat " + grantName + " " + totalRoundsNeededToWin + " times with a total of " + userHealthDefault + " health?  We shall see!");
+		return true;
+	} else {
+		window.alert("You have indicated that you do not wish to play... or cannot follow simple Y/N prompts effectively.  Either way, you shall be no match for " + grantName + "!");
+		return false;
+	}
+}
+
+// startCombat() includes the full battle loop, which consists of two nested while loops (one for the game and one for each round of combat within the game).
 
 function startCombat() {
 
-  while (userWins < pointsNeededToWin && userHealth > 0) {// Rounds needed to win
+	// Set initial health settings
 
-    while (userHealth >= healthDepleted && grantHealth >= healthDepleted) {
+	var userHealthTotal = userHealthDefault;
+  	var grantHealthTotal = grantHealthDefault;
+  	var minHealth = 0;
 
-      if (window.confirm('Would you like to continue Attacking?')) {
-        console.log(userName + ' decides to attack.');
-      } else {
-        console.log(userName + ' has ended the game.')
-        return;
-      }
+  	// Set initial win totals...
 
-      var grantAttack = getDamage();
-      var userAttack = getDamage();
+  	var userWinTotal = 0;
+	var grantWinTotal = 0;
 
-      grantHealth = grantHealth - userAttack;
-      userHealth = userHealth - grantAttack;
+	// declare and set initial round number...
 
-      console.log(userName + ' has ' + userHealth + ' health..');
-      console.log(grant + ' has ' + grantHealth + ' health..');
+	var roundNumber = 0;
 
-      }
+  	// This "outer" while loop ensures that the game continues until either the total number of rounds needed to win is achives, or the user's health reaches the min value.
 
-      console.log('round over')
-      userWins++;
-      grantHealth = 10;
+	while (userWinTotal < totalRoundsNeededToWin && userHealthTotal > minHealth) {
 
-    }
+		// Prep for new round - Grant's health is reset to default; # of turns is reset to zero, and the round number is increased by one.
 
-  if (userHealth < healthDepleted) {
-    console.log(userName + ' Lost');
-  } else {
-    console.log(grant + ' Lost');
-  }
-  console.log('game over');
+		grantHealthTotal = grantHealthDefault;
+		var turnNumber = 0;
+		roundNumber = roundNumber + 1;
 
-  grantHealth = 10;
-  userHealth = 40;
-  userWins = 0;
+		console.log("ROUND " + roundNumber);
+		console.log("Current health totals:  " + userName + " " + userHealthTotal + "; " + grantName + " " + grantHealthTotal + ".");
+		console.log("Current total round wins for " + userName + ":  " + userWinTotal + ".");
+
+		// This "inner" while loop ensures that the round continues until either Grant or the user's health total reaches the min value.
+
+		while (userHealthTotal > minHealth && grantHealthTotal > minHealth) {
+
+			// The following if else statement offer the choice to quit after each turn.  It will not kick in until after turn 1 has occurred, since the user just agreed to play.
+
+			if (turnNumber !== 0) {
+
+	  			if (window.confirm("Had enough?  Should " + userName + " continue to fight " + grantName + "?") === false) {
+	  				console.log("You cowardly cur!");
+	  				doYouWantToContinue = false;
+	  				return
+	  			} else {
+	  				console.log("The battle continues!");
+	  				doYouWantToContinue = true;
+	  			}
+			
+			}
+
+			turnNumber = turnNumber + 1;
+
+			console.log("Turn " + turnNumber + ".  " + userName + " and " + grantName + " both attack!");
+
+			var userDamage = getDamage(minDamage, maxDamage);
+			grantHealthTotal = grantHealthTotal - userDamage;
+			var grantDamage = getDamage(minDamage, maxDamage);
+			userHealthTotal = userHealthTotal - grantDamage;
+
+			if (userDamage === 0) {
+				console.log("Bummer!  " + userName + " misses " + grantName + ", dealing " + userDamage + " damage.  " + grantName + " still has " + grantHealthTotal + " health.");
+			} else {
+				console.log("YES!  " + userName + " deals " + userDamage + " damage of pain to " + grantName + "!  " + grantName + " now has " + grantHealthTotal + " health.");
+			}
+
+			if (grantDamage === 0) {
+				console.log("Ha!  " + grantName + " misses " + userName + " completely, dealing " + grantDamage + " damage.  " + userName + " still has " + userHealthTotal + " health.");
+			} else {
+				console.log("Ouch!  " + grantName + " deals " + grantDamage + " damage to " + userName + ".  " + userName + " now has " + userHealthTotal + " health.");
+			}
+		  
+		}
+
+		// Round winner determination
+
+		if (userHealthTotal > grantHealthTotal) {
+			userWinTotal = userWinTotal + 1;
+			console.log(grantName + " has been reduced to " + grantHealthTotal + " health.  " + userName + " wins the round!  Awesome!");
+		} else  if (grantHealthTotal > userHealthTotal) {
+			grantWinTotal = grantWinTotal + 1;
+			console.log(grantName + " emerges victorious.  How can this be?");
+		} else {
+			console.log("No one wins.  The round ends in a draw, " + userHealthTotal + " to " + grantHealthTotal + ".");
+		}
+
+		console.log("------------------------");
+
+	}
+
+    // Game winner determination
+
+  	if (userWinTotal === totalRoundsNeededToWin) {
+  		gameWinner = userName;
+  	} else if (userHealthTotal <= minHealth) {
+  		gameWinner = grantName;
+  	} else {
+  		gameWinner = "No one";
+  	}
+  	
+  	return
 
 }
-
-
-function startGame() {
-
-  var gameStartPrompt = prompt ('Do you dare enter the Dungeon?');
-  var consent = 'yes';
-  var checkGamePrompt = gameStartPrompt === consent;
-  var playFalse = 'Rerun code to play game.';
-
-  if (checkGamePrompt !== true) {
-
-      console.log(playFalse);
-
-    } else {
-      userName = prompt('Enter your wizarding name!');
-
-      console.log(userName + ' enters the dungeon...');
-
-      startCombat();
-    }
-
-}
+	
