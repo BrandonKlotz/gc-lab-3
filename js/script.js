08bd4ab1bb138dc4d72477ae718e4c141eb517d6
Sloth I <github@slothwerks.com>
2017-10-25 22:36:41 -0400

    Updated ID for user-heal-total in index.html and started overhauling the JavaScript file.  Opening game functions in place and (sort of) working.

--- js/script.js
+++ js/script.js
@@ -1,49 +1,127 @@
-var user = {
-name: '',
-health: 40,
-wins: 0,
-attack: getNum(1,3),
-heal: 0,
-healCount: 0
-}
-
-var compPlayer = {
-name: 'Grant Chirpus',
-health:  10,
-wins: 0,
-attack: getNum(1,5)
-}
+// define global game variables and assign values
 
 let pointsNeededToWin = 3;
 let healthDepleted = 0;
 
-function startGame(playerOne, playerTwo) {
+// getUserName() requires a name of not null value.  It occurs as a pop-up window prompt.
 
-  var gameStartPrompt = prompt ('Do you dare enter the Dungeon?');
-  var consent = 'yes';
-  var checkGamePrompt = gameStartPrompt === consent;
-  var playFalse = 'Rerun code to play game.';
-
-  if (checkGamePrompt !== true) {
-
-      console.log(playFalse);
-
+function getUserName() {
+  do {
+    var getName = prompt("Please enter your Wizard name.");
+    if (getName !== null) {
+      return getName;
     } else {
-      playerOne.name = prompt('Enter your wizarding name!');
+      window.alert("Please enter a name to continue.");
+    }
+  }
+  while (getName === null);
+}
 
-      if (playerOne.name !== null) {
+// startGame() now must now be responsible for the creation of the user and computer objects.  It is initiated when the Start button is clicked.
 
-        console.log(playerOne.name + ' enters the dungeon...');
+var startButtonClick = document.getElementById("start");
+startButtonClick.onclick = function () {
+  startGame();
+}
 
-        startCombat(playerOne, playerTwo);
+function startGame() {
 
-      } else {
-        playerOne.name = prompt('You did not enter a name.. Enter your wizarding name!');
-        return;
-      }
+  // define objects for user character and computer character (alias Grant)
+ 
+  var user = {
+    name: '', // A string which will include the user's name
+    health: 40, // Starting health total
+    wins: 0, // Total number of wins
+    attack: getNum(1, 3), // Attack power - random number between x and y (inclusive)
+    heal: getNum(1, 10), // Heal power - random number between x and y (inclusive)
+    healCount: 2, // Total number of heals available per game
+  };
+
+  var compPlayer = {
+    name: 'Grant Chirpus', // Default name of computer character
+    health:  10, // Starting health total
+    wins: 0, // Total number of wins
+    attack: getNum(1, 5), // Attack power - random number between x and y (inclusive)
+  };
+
+  // Once the game has started, the Start button should be removed from the HTML DOM.
+
+  user.name = getUserName();
+
+  console.log(user.name);
+
+  // At this point, the code should print the current stats to the console.  Then print text to the console text area?
+
+  The block below was a function but for some reason it won't work when defined outside of this function even when it's called from within this function.
+
+  console.log(compPlayer.name);
+  
+  var userName = document.getElementById("user-name");
+  update(userName, user.name);
+  var userHealth = document.getElementById("user-health");
+  update(userHealth, user.health);
+  var userHealCount = document.getElementById("user-heal-count");
+  update (userHealCount, user.healCount); 
+  var userWins = document.getElementById("user-wins");
+  update(userWins, user.wins);
+  var computerName = document.getElementById("computer-name");
+  update (computerName, compPlayer.name);
+  var computerHealth = document.getElementById("computer-health");
+  update(computerHealth, compPlayer.health);
+
+  function update(id, stat) {
+      id.innerHTML = stat;
+    }
+
+// printStats();
+
+printConsoleText("Can you defeat " + compPlayer.name + " a total of " + pointsNeededToWin + " times?  Select an action to begin!")
+
+}
+
+// printStats() is designed to update the curent stats to the web game console interface.
+
+function printStats() {
+
+  console.log(user);
+  console.log(compPlayer);
+  
+  var userName = document.getElementById("user-name");
+  update(userName, user.name);
+  var userHealth = document.getElementById("user-health");
+  update(userHealth, user.health);
+  var userHealCount = document.getElementById("user-heal-count");
+  update (userHealCount, user.healCount); 
+  var userWins = document.getElementById("user-wins");
+  update(userWins, user.wins);
+  var computerName = document.getElementById("computer-name");
+  update (computerName, compPlayer.name);
+  var computerHealth = document.getElementById("computer-health");
+  update(computerHealth, compPlayer.health);
+
+  function update(id, stat) {
+      id.innerHTML = stat;
     }
 
 }
+
+// printConsoleText() takes a string of text and prints it to the web game console interface.
+
+function printConsoleText(text) {
+  var console = document.getElementById("console");
+  console.innerHTML = text;
+}
+
+// getNum() generates a random integer between min and max.
+
+function getNum(min, max) {
+  var min = Math.ceil(min);
+  var max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/*
+
 
 function attack(playerOne, playerTwo) {
 
@@ -68,14 +146,6 @@ function magicPotion(playerOne) {
   }
   return;
 
-}
-
-function getNum(min, max) {
-  var min;
-  var max;
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random(min, max) * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
 function endGame(playerOne, playerTwo) {
@@ -150,3 +220,5 @@ function startCombat(playerOne, playerTwo) {
 
     endGame(playerOne, playerTwo);
 }
+
+*/
